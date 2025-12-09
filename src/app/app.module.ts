55c597fcaa49e535cc92b8ccd56bad87366623fe
Asaf Regev <asaf.regev@shieldfc.com>
2025-12-09 11:38:05 +0200

    Initialize project with Angular dependencies, update locale settings to French, and adjust date formatting in the app component.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,18 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { NgModule, LOCALE_ID } from '@angular/core';
+import { CommonModule, registerLocaleData } from '@angular/common';
 import { BrowserModule } from '@angular/platform-browser';
+import localeFr from '@angular/common/locales/fr';
 
 import { AppComponent } from './app.component';
 import { ChildComponent } from './child/child.component';
-import { MAT_DATE_LOCALE } from '@angular/material/core';
+
+// Register French locale data
+registerLocaleData(localeFr);
 
 @NgModule({
   bootstrap: [AppComponent],
   declarations: [AppComponent, ChildComponent],
   imports: [CommonModule, BrowserModule],
-  providers: [{ provide: MAT_DATE_LOCALE, useValue: 'ja-JP' }],
+  providers: [{ provide: LOCALE_ID, useValue: 'fr-FR' }],
 })
 export class AppModule {}
